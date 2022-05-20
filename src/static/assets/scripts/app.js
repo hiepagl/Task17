3f41a5cec376f4437193b13523d81f9a0696163c
DESKTOP-OGLLSNF\AGL <nguyen_xuan_hiep@allgrow-labo.jp>
2022-05-20 17:24:00 +0700

    2nd commit

--- src/static/assets/scripts/app.js
+++ src/static/assets/scripts/app.js
@@ -7,3 +7,26 @@ $('.c-slider__list').slick({
     autoplay: true,
     arrows: false,
 });
+
+$('.c-hero__list').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerPadding: '0',
+    centerMode: true,
+    variableWidth: true,
+    arrows: true,
+    autoplay: true,
+    prevArrow: "<button type='button' class='c-hero__btnLeft'><figure><img src='./assets/images/hero/arrowLeft.png' alt='arrowLeft'></figure></button>",
+    nextArrow: "<button type='button' class='c-hero__btnRight'><figure><img src='./assets/images/hero/arrowRight.png' alt='arrowRight'></figure></button>",
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                arrows: false,
+            }
+        }
+    ]
+});
+
