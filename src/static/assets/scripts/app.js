a1488640dba4b263ac9458c05d1ec113d70c92e5
DESKTOP-OGLLSNF\AGL <nguyen_xuan_hiep@allgrow-labo.jp>
2022-05-25 17:19:32 +0700

    4th commit

--- src/static/assets/scripts/app.js
+++ src/static/assets/scripts/app.js
@@ -4,7 +4,7 @@ $('.c-slider__list').slick({
     speed: 500,
     fade: true,
     cssEase: 'linear',
-    autoplay: false,
+    autoplay: true,
     arrows: false,
 });
 
@@ -12,12 +12,12 @@ $('.c-hero__list').slick({
     dots: true,
     infinite: true,
     speed: 300,
-    slidesToShow: 1,
+    slidesToShow: 3,
     centerPadding: '0',
     centerMode: true,
     variableWidth: true,
     arrows: true,
-    autoplay: false,
+    autoplay: true,
     prevArrow: "<button type='button' class='c-hero__btnLeft'><figure><img src='./assets/images/hero/arrowLeft.png' alt='arrowLeft'></figure></button>",
     nextArrow: "<button type='button' class='c-hero__btnRight'><figure><img src='./assets/images/hero/arrowRight.png' alt='arrowRight'></figure></button>",
     responsive: [
